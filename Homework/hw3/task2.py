a1520295b6deb2062f00836a29811ddc36438125
rkorostin <rkorostin@gmail.com>
2023-09-05 10:03:15 +0300

    Комментарии по коду. Добавил запрос на ввод данных для пользователя

--- Homework/hw3/task2.py
+++ Homework/hw3/task2.py
@@ -1,50 +1,46 @@
-import re
-from collections import Counter
-import requests
-from bs4 import BeautifulSoup
-
-
-def count_words(text):
-    """
-    Функция count_words принимает текст и производит подсчет слов,
-    которые имеют более 3-х символов
-    """
-    # Удаляем знаки препинания и приводим текст к нижнему регистру
-    text = re.sub(r'[^\w\s]', '', text.lower())
-
-    # Возвращаем список слов, удовлетворяющих регулярному выражению
-    words = re.findall(r'\b\w+\b', text)
-
-    #Подсчитываем количество слов, которые имеют длину больше 3-х символов.
-    words = [word for word in words if len(word) > 3]
-
-    # Подсчитываем количество встречаемых слов
-    word_counts = Counter(words)
-
-    # Возвращаем 10 самых частых слов
-    return word_counts.most_common(10)
+import requests  # для отправки HTTP-запросов
+from bs4 import BeautifulSoup  # для парсинга HTML
+from collections import Counter  # для подсчета количества элементов в списке
+import re  # для работы с регулярными выражениями
 
 
 def get_text_from_url(url):
     """
-    Функция get_text_from_url принимает URL-адрес и возвращает ответ в виде текста HTML.
+    Функция для получения текста с веб-страницы по заданному URL-адресу
     """
-    response = requests.get(url)
-    return response.text
+    response = requests.get(url)  # Отправляем GET-запрос по указанному URL-адресу
+    soup = BeautifulSoup(response.content,
+                         'html.parser')  # Создаем объект BeautifulSoup для парсинга HTML-кода страницы
+    text_url = soup.get_text()  # Получаем текст со страницы
+    return text_url
 
 
-def parse_text(html):
+def parse_text(html_text):
     """
-    Функция parse_text принимает текст HTML, парсит его и возвращает текст
+    Функция для обработки текста: удаление символов и приведение к нижнему регистру
     """
-    soup = BeautifulSoup(html, 'html.parser')
-    text = soup.get_text()
-    return text
+    html_text = re.sub(r'\W+', ' ', html_text)  # Удаляем все символы кроме букв и цифр
+    html_text = html_text.lower()  # Приводим все символы к нижнему регистру
+    return html_text
 
 
-url = "https://docs.python.org/3.11/library/urllib.request.html?highlight=request#module-urllib.request"
-html = get_text_from_url(url)
-text = parse_text(html)
+def count_words(text, max_word_length):
+    """
+    Функция для подсчета наиболее часто встречающихся слов в тексте
+    """
+    words = text.split()  # Разбиваем текст на слова
+    filtered_words = [word for word in words if
+                      len(word) > max_word_length]  # Оставляем слова, длина которых больше заданного значения
+    word_counts = Counter(filtered_words)  # Подсчитываем количество каждого слова
+    most_popular_words = word_counts.most_common(10)  # Получаем 10 наиболее часто встречающихся слов
+    return most_popular_words
 
-most_common_words = count_words(text)
-print(most_common_words)
+
+url = input("Введите URL-адрес: ")
+max_word_length = int(input("Введите максимальное количество символов в слове: "))
+
+html = get_text_from_url(url)  # Получаем HTML-текст по указанному URL-адресу
+text = parse_text(html)  # Обрабатываем полученный текст
+popular_words = count_words(text, max_word_length)  # Подсчитываем наиболее часто встречающиеся слова
+
+print(popular_words)
