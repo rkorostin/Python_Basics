357d53a3c0e73b54e821223d85f6731bd57ee13e
rkorostin <rkorostin@gmail.com>
2023-09-08 13:01:07 +0300

    Добавил комментарии и вывод изначальной матрицы

--- Homework/hw4/task1.py
+++ Homework/hw4/task1.py
@@ -1,15 +1,26 @@
 def transpose_matrix(matrix):
+    # Создаем пустую матрицу для хранения транспонированной матрицы
     transposed_matrix = []
 
-    for i in range(len(matrix[0])):
+    # Выводим исходную матрицу
+    print("Исходная матрица:")
+    for row in matrix:
+        print(row)
 
+    # Используем циклы for для прохода по столбцам и строкам исходной матрицы
+    for i in range(len(matrix[0])):
+        # Создаем пустую строку для текущего столбца транспонированной матрицы
         transposed_row = []
 
         for j in range(len(matrix)):
+            # Добавляем элемент из текущего столбца исходной матрицы в текущую строку транспонированной матрицы
             transposed_row.append(matrix[j][i])
 
+        # Добавляем текущую строку транспонированной матрицы в общую матрицу
         transposed_matrix.append(transposed_row)
 
+    # Выводим транспонированную матрицу
+    print("Транспонированная матрица:")
     for row in transposed_matrix:
         print(row)
 
