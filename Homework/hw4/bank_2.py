8f7958c2a152d4e26d3c88c65931bc7a535d7fad
rkorostin <rkorostin@gmail.com>
2023-09-11 18:04:10 +0300

    Добавил отдельные счетчики операций для снятия и пополнения

--- Homework/hw4/bank_2.py
+++ Homework/hw4/bank_2.py
@@ -8,26 +8,27 @@ def bankomat():
     Каждая операция вносится в список, который в любой момент можно вывести в терминале
     """
     balance = 0
-    count = 0
+    count_add = 0
+    count_withdraw = 0
     operations = []
 
     def add_balance(balance, amount):
         """
         Функция пополнения счёта. Принимает текущий баланс и пополняемую сумму
         """
-        nonlocal count  # 'count' является нелокальной переменной и должна быть доступна
+        nonlocal count_add  # 'count' является нелокальной переменной и должна быть доступна
         # из внешней области видимости.
         if amount % 50 == 0:  # Если введенная сумма (amount) делится на 50 без остатка,
             # то она добавляется к текущему балансу (balance)
             balance += amount
-            count += 1
+            count_add += 1
             operations.append("Пополнение на сумму: " + str(amount))  # Логирование операций
             if balance > 5000000:  # если баланс более 5_000_000, то вычитается налог на богатство 10%
                 tax = 0.1 * amount
                 balance -= tax
                 print("Налог 10% на богатство: ", tax)
                 operations.append("Налог на богатство: " + str(tax))
-            if count % 3 == 0:  # Действие для каждой третьей операции
+            if count_add % 3 == 0:  # Действие для каждой третьей операции
                 commission = min(0.03 * amount, 600)  # Комиссия за пополнение счёта 3%,
                 # но не менее 30 и не более 600
                 if commission < 30:
@@ -39,13 +40,13 @@ def bankomat():
                 operations.append("Комиссия за каждую третью операцию пополнения: " + str(commission))
         else:
             print("Сумма пополнения должна быть кратна 50")
-        return balance, count, operations
+        return balance, count_add, operations
 
     def withdraw_balance(balance, amount):
         """
         Функция снятия со счёта. Принимает текущий баланс и сумму для снятия
         """
-        nonlocal count
+        nonlocal count_withdraw
         if amount % 50 == 0:
             if amount <= balance:  # сумма снятия должна быть не меньше баланса
                 if balance > 5000000:
@@ -53,14 +54,14 @@ def bankomat():
                     balance -= tax
                     print("Налог 10% на богатство: ", tax)
                     operations.append("Налог на богатство: " + str(tax))
-                elif count % 3 == 0:
+                elif count_withdraw % 3 == 0:
                     commission = min(0.03 * amount, 600)
                     if commission < 30:
                         commission = 30
                     elif commission > 600:
                         commission = 600
-                    balance -= commission
-                    count += 1
+                    balance -= (amount + commission)
+                    count_withdraw += 1
                     print("Комиссия 3% за каждую третью операцию: ", commission)
                     operations.append("Комиссия за каждую третью операцию снятия: " + str(commission))
                 else:
@@ -71,7 +72,7 @@ def bankomat():
                     elif commission > 600:
                         commission = 600
                     balance -= (amount + commission)
-                    count += 1
+                    count_withdraw += 1
                     print("Снято: ", amount)
                     print("Комиссия 1.5% за операцию: ", commission)
                     operations.append("Снято средств: " + str(amount))
@@ -80,7 +81,7 @@ def bankomat():
                 print("Недостаточно средств на счете")
         else:
             print("Сумма снятия должна быть кратна 50")
-        return balance, count, operations
+        return balance, count_withdraw, operations
 
     def show_operations(operations):
         """
@@ -96,11 +97,11 @@ def bankomat():
 
         if action == "1":
             amount = int(input("Введите сумму пополнения: "))
-            balance, count, operations = add_balance(balance, amount)
+            balance, count_add, operations = add_balance(balance, amount)
 
         elif action == "2":
             amount = int(input("Введите сумму снятия: "))
-            balance, count, operations = withdraw_balance(balance, amount)
+            balance, count_withdraw, operations = withdraw_balance(balance, amount)
 
         elif action == "3":
             show_operations(operations)
