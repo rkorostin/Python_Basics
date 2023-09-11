b1bf3a40f392eb4f768cafcb22420a35e5ee77d0
rkorostin <rkorostin@gmail.com>
2023-09-11 13:42:57 +0300

    Добавил налог на богатство

--- Homework/hw4/bank.py
+++ Homework/hw4/bank.py
@@ -12,6 +12,10 @@ def bankomat():
                 # то она добавляется к текущему балансу (balance)
                 balance += amount
                 count += 1
+                if balance > 5000000:  # если баланс более 5_000_000, то вычитается налог на богатство 10%
+                    tax = 0.1 * amount
+                    balance -= tax
+                    print("Налог 10% на богатство: ", tax)
                 if count % 3 == 0:  # Действие для каждой третьей операции
                     commission = min(0.03 * amount, 600)  # Комиссия за пополнение счёта 3%,
                     # но не менее 30 и не более 600
@@ -20,14 +24,18 @@ def bankomat():
                     elif commission > 600:
                         commission = 600
                     balance -= commission
-                    print("Комиссия за каждую третью операцию: ", commission)
+                    print("Комиссия 3% за каждую третью операцию: ", commission)
             else:
-                print("Сумма пополнения должна быть кратна 50 у.е.")
+                print("Сумма пополнения должна быть кратна 50")
 
         elif action == "2":
             amount = int(input("Введите сумму снятия: "))
             if amount % 50 == 0:
                 if amount <= balance:  # сумма снятия должна быть не меньше баланса
+                    if balance > 5000000:
+                        tax = 0.1 * amount
+                        balance -= tax
+                        print("Налог 10% на богатство: ", tax)
                     commission = min(0.015 * amount, 600)  # комиссия за снятие 1,5%,
                     # но не менее 30 и не более 600
                     if commission < 30:
@@ -37,7 +45,7 @@ def bankomat():
                     balance -= (amount + commission)
                     count += 1
                     print("Снято: ", amount)
-                    print("Комиссия за операцию: ", commission)
+                    print("Комиссия 1.5% за операцию: ", commission)
                     if count % 3 == 0:
                         commission = min(0.03 * amount, 600)
                         if commission < 30:
@@ -45,11 +53,11 @@ def bankomat():
                         elif commission > 600:
                             commission = 600
                         balance -= commission
-                        print("Комиссия за каждую третью операцию: ", commission)
+                        print("Комиссия 3% за каждую третью операцию: ", commission)
                 else:
                     print("Недостаточно средств на счете")
             else:
-                print("Сумма снятия должна быть кратна 50 у.е.")
+                print("Сумма снятия должна быть кратна 50")
 
         elif action == "0":
             break
