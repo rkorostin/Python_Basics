f68a23bd338d8ec33abbbed70697784f873386b2
rkorostin <rkorostin@gmail.com>
2023-09-11 17:36:41 +0300

    Переделал логику списания процентов (или 1.5 или 3). Проценты не списываются, если присутствует налог на богатство

--- Homework/hw4/bank_2.py
+++ Homework/hw4/bank_2.py
@@ -45,19 +45,7 @@ def bankomat():
                     balance -= tax
                     print("Налог 10% на богатство: ", tax)
                     operations.append("Налог на богатство: " + str(tax))
-                commission = min(0.015 * amount, 600)  # комиссия за снятие 1,5%,
-                # но не менее 30 и не более 600
-                if commission < 30:
-                    commission = 30
-                elif commission > 600:
-                    commission = 600
-                balance -= (amount + commission)
-                count += 1
-                print("Снято: ", amount)
-                print("Комиссия 1.5% за операцию: ", commission)
-                operations.append("Снято средств: " + str(amount))
-                operations.append("Комиссия за операцию снятия: " + str(commission))
-                if count % 3 == 0:
+                elif count % 3 == 0:
                     commission = min(0.03 * amount, 600)
                     if commission < 30:
                         commission = 30
@@ -66,6 +54,28 @@ def bankomat():
                     balance -= commission
                     print("Комиссия 3% за каждую третью операцию: ", commission)
                     operations.append("Комиссия за каждую третью операцию снятия: " + str(commission))
+                else:
+                    commission = min(0.015 * amount, 600)  # комиссия за снятие 1,5%,
+                    # но не менее 30 и не более 600
+                    if commission < 30:
+                        commission = 30
+                    elif commission > 600:
+                        commission = 600
+                    balance -= (amount + commission)
+                    count += 1
+                    print("Снято: ", amount)
+                    print("Комиссия 1.5% за операцию: ", commission)
+                    operations.append("Снято средств: " + str(amount))
+                    operations.append("Комиссия за операцию снятия: " + str(commission))
+                # if count % 3 == 0:
+                #     commission = min(0.03 * amount, 600)
+                #     if commission < 30:
+                #         commission = 30
+                #     elif commission > 600:
+                #         commission = 600
+                #     balance -= commission
+                #     print("Комиссия 3% за каждую третью операцию: ", commission)
+                #     operations.append("Комиссия за каждую третью операцию снятия: " + str(commission))
             else:
                 print("Недостаточно средств на счете")
         else:
