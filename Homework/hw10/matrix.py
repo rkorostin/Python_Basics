dfa67fb82bf2e26601288b52b4d66980a513422b
rkorostin <rkorostin@gmail.com>
2023-10-01 20:57:02 +0300

    Транспонирование матрицы

---
+++ Homework/hw10/matrix.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*
+class Matrix:
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def transpose(self):
+        # Создаем пустую матрицу для хранения транспонированной матрицы
+        transposed_matrix = []
+
+        # Выводим исходную матрицу
+        print("Исходная матрица:")
+        for row in self.matrix:
+            print(row)
+
+        # Используем циклы for для прохода по столбцам и строкам исходной матрицы
+        for i in range(len(self.matrix[0])):
+            # Создаем пустую строку для текущего столбца транспонированной матрицы
+            transposed_row = []
+
+            for j in range(len(self.matrix)):
+                # Добавляем элемент из текущего столбца исходной матрицы в текущую строку транспонированной матрицы
+                transposed_row.append(self.matrix[j][i])
+
+            # Добавляем текущую строку транспонированной матрицы в общую матрицу
+            transposed_matrix.append(transposed_row)
+
+        # Выводим транспонированную матрицу
+        print("Транспонированная матрица:")
+        for row in transposed_matrix:
+            print(row)
+
+
+matrix = Matrix([[1, 2, 3, 4],
+                 [5, 6, 7, 8],
+                 [9, 8, 7, 6],
+                 [5, 4, 3, 2]])
+
+matrix.transpose()
